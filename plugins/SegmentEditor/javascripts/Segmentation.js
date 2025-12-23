1b6e7c5a03cbb10d09a6265d01fe01f0bd7a9fd1
Thomas ZILLIOX <thomas.zilliox@innocraft.com>
2025-12-23 02:23:58 +0100

    Update UI screenshots and fix tests

--- plugins/SegmentEditor/javascripts/Segmentation.js
+++ plugins/SegmentEditor/javascripts/Segmentation.js
@@ -521,7 +521,9 @@ Segmentation = (function($) {
                 const comparisonService = window.CoreHome.ComparisonsStoreInstance;
                 const segmentDefinition = $button.closest('li').data('definition');
                 if ($button.attr('data-state') === 'active') {
-                  comparisonService.removeSegmentComparisonByDefinition(segmentDefinition);
+                  if (segmentDefinition) {
+                    comparisonService.removeSegmentComparisonByDefinition(segmentDefinition);
+                  }
                 } else {
                   comparisonService.addSegmentComparison({
                     segment: segmentDefinition,
